3f15777ecd6ade86306401c15417d14b4d104e8d
William Damm <wdamm@powerhrg.com>
2020-03-21 00:39:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,30 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-puts number
+
+div3 = number / 3.to_f
+div5 = number / 5.to_f
+
+r3 = number / 3.round()
+r5 = number / 5.round()
+
+
+if div3 == r3 && div5 == r5
+return "FizzBuzz"
+puts "FizzBuzz"
+
+elsif div3 == r3
+
+return "Fizz"
+puts "Fizz"
+
+elsif div5 == r5
+return "Buzz"
+puts "Buzz"
+
+
+
+end
+
+
 end
